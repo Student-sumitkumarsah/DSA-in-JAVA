6d1fedb1436967884b9bbc1827e5ed2399813c67
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-02 22:00:44 +0530

    dataType

---
+++ DAY1/dataType.java
@@ -0,0 +1,17 @@
+package DSA.DAY1;
+
+public class dataType {
+    public static void main(String args[]){
+        byte b=3;
+        System.out.println(b);
+        char ch='a';
+        System.out.println(ch);
+        boolean var =true;
+        int num=2;
+        //long   use biggest integer number 
+        // double use biggest decimal number
+        short d=3;
+        float price = 4.3;
+    }
+    
+}
