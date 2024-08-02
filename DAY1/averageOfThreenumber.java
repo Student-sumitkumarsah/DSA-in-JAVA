33571c680bc839f4ce8ef71733953a2c3916a221
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-02 23:11:52 +0530

    averageOfThreeNumber

---
+++ DAY1/averageOfThreenumber.java
@@ -0,0 +1,15 @@
+package DSA.DAY1;
+import java.util.*;
+public class averageOfThreenumber {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the first number = ");
+        int a=sc.nextInt();
+        System.out.print("Enter the second number = ");
+        int b=sc.nextInt();
+        System.out.print("Enter the third number = ");
+        int c=sc.nextInt();
+        System.out.println("Average = "+(a+b+c)/3);
+    }
+    
+}
