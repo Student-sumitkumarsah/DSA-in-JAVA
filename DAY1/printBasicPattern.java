e460ae8947508f25fe2fd1bbf3597410f72893c0
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-02 21:33:30 +0530

    basicPattern

---
+++ DAY1/printBasicPattern.java
@@ -0,0 +1,12 @@
+package DSA.DAY1;
+
+public class printBasicPattern {
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        
+    }
+    
+}
