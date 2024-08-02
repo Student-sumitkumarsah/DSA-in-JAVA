d131738971d23f0a5c65d06cf25881de37a44b02
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-02 22:33:29 +0530

    areaOfCircle

---
+++ DAY1/areaOfCircle.java
@@ -0,0 +1,12 @@
+package DSA.DAY1;
+import java.util.*;
+public class areaOfCircle {
+    public static void main(String args[]){
+    Scanner sc =new Scanner(System.in);
+    System.out.print("Enter the radius of circle = ");
+    double pi=3.14;
+    int r =sc.nextInt();
+    System.out.print("Area of circle = "+(pi*r*r));
+
+    }
+}
