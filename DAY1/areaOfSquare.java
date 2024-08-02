415a3a8aa9229189398bf3bf1e80386c91d27ae5
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-02 23:18:00 +0530

    areaOfSquare

---
+++ DAY1/areaOfSquare.java
@@ -0,0 +1,11 @@
+package DSA.DAY1;
+import java.util.*;
+public class areaOfSquare {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the side of square = ");
+        int a=sc.nextInt();
+        System.out.println("Area of square = "+(a*a));
+    }
+    
+}
