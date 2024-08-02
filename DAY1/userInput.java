8176be58f6030a6c6d44567ce61e6561047dd8bf
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-02 22:24:00 +0530

    userInput

---
+++ DAY1/userInput.java
@@ -0,0 +1,33 @@
+package DSA.DAY1;
+import java.util.*;
+public class userInput {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first number = ");
+        int a=sc.nextInt();
+        System.out.print("Enter the Second number = ");
+        int b=sc.nextInt();
+        System.out.println("Sum of two number = " + (a+b));
+
+    }
+    /*
+     * next (For single string)
+     * 
+     * nextline (For unlimited string)
+     * 
+     * nextInt (for integer)
+     * 
+     * nextByte (for integer)
+     * 
+     * nextFloat( for decimal num)
+     * 
+     * nextDouble (for biggest decimal num)
+     * 
+     * nextBoolean (for true and false)
+     * 
+     * nextShort (for integer)
+     * 
+     * nextLong( for biggest integer)
+     */
+    
+}
