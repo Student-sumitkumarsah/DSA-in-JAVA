8d1d1099d36fddc022f10b3773ecc0cfa1cc7a1a
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-05 22:26:56 +0530

    Positive or Negative

---
+++ DAY2/positiveOrNegative.java
@@ -0,0 +1,16 @@
+package DSA.DAY2;
+import java.util.*;
+public class positiveOrNegative {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        int a=sc.nextInt();
+        if(a>=0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+    
+}
