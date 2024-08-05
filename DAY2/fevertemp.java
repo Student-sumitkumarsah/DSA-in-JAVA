12fec6a20a2798925d1a942be34f5d2233e9b2cf
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-05 22:31:02 +0530

    Fever or not

---
+++ DAY2/fevertemp.java
@@ -0,0 +1,14 @@
+package DSA.DAY2;
+
+public class fevertemp {
+    public static void main(String[] args) {
+        double temp=103.5;
+        if(temp>100){
+            System.out.println("You have a fever ");
+        }
+        else{
+            System.out.println("You don't have a fever");
+        }
+        
+    }
+}
