5f01cd642328cd96f930fe9ce5288ef1b5400a9b
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-06 21:00:19 +0530

    Print Number Using While Loop

--- DAY3/printNumber1to10.java
+++ DAY3/printNumber1to10.java
@@ -1,9 +1,12 @@
 package DSA.DAY3;
-
+import java.util.*;
 public class printNumber1to10 {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the range = ");
+        int a=sc.nextInt();
         int num=1;
-        while(num<=10){
+        while(num<=a){
             System.out.println(num);
             num++;
         }
