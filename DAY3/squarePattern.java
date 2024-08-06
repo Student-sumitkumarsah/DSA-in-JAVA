5d8cf56f0e314e7ba1f72d3cef71da374b6964f8
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-06 21:19:06 +0530

    Square Pattern Using For Loop

---
+++ DAY3/squarePattern.java
@@ -0,0 +1,10 @@
+package DSA.DAY3;
+
+public class squarePattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            System.out.println("****");
+        }
+    }
+    
+}
