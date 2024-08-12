2b584670f09b0dd86720fa9f3fa0020822858c23
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-12 22:30:51 +0530

    Pattern 2

---
+++ DAY4/pattern2.java
@@ -0,0 +1,17 @@
+package DSA.DAY4;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    
+    
+    
+    
+}
+}
