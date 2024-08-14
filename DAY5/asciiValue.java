2f67e1eb84e7cfb4b15384054149f691d2bc9f1e
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-14 23:09:19 +0530

    ALL ASCII value

---
+++ DAY5/asciiValue.java
@@ -0,0 +1,12 @@
+package DSA.DAY5;
+
+public class asciiValue {
+    public static void main(String[] args) {
+        
+    
+    for(int i=1;i<=255;i++){
+        System.out.println((char)i);
+    }
+    
+}
+}
