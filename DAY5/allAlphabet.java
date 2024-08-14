493c294ec01e5dbcbe1bb8dbcb7f1aa955fd69d4
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-14 22:05:51 +0530

    A to Z Alphabet

---
+++ DAY5/allAlphabet.java
@@ -0,0 +1,12 @@
+package DSA.DAY5;
+
+public class allAlphabet {
+    public static void main(String[] args) {
+        char ch='a';
+        for(char i=1; i<=26;i++){
+            System.out.println(ch);
+            ch++;
+        }
+    }
+    
+}
