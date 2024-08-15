e7dea2d987279b7a8843f049cfd2432f7e1b7b72
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-15 22:18:13 +0530

    Sum of Between the 100 to 200 Divisible By 9

---
+++ DAY6/divisibleBy9.java
@@ -0,0 +1,15 @@
+package DSA.DAY6;
+
+public class divisibleBy9 {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=100;i<=200;i++){
+            if(i%9==0){
+                sum=sum+i;
+            }
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
