a4f963ec89db8f9b7043e7fa5b88f3859f90b42d
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-18 21:44:02 +0530

    Print Hello World Using Java

---
+++ DAY7/function.java
@@ -0,0 +1,12 @@
+package DSA.DAY7;
+
+public class function {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+         printHelloWorld();
+        
+    }
+    
+}
