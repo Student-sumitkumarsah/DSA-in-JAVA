56176b4bab12a60ea7b2db2fbef25c2014e70e46
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-19 21:59:59 +0530

    Function Overloading using Type

---
+++ DAY8/diffDataTypeOverloading.java
@@ -0,0 +1,17 @@
+package DSA.DAY8;
+
+public class diffDataTypeOverloading {
+    public static int  sum(int a,int b){
+         return a+b;
+
+    }
+    public static float sum(float a, float b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(2,4));
+        System.out.println(sum(2f,4f));
+        
+    }
+    
+}
