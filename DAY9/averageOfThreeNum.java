bfed453dc95bc2375b6b1b990c83019dd4a5d39c
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-20 21:31:27 +0530

    Average Of Three number

---
+++ DAY9/averageOfThreeNum.java
@@ -0,0 +1,14 @@
+package DSA.DAY9;
+
+public class averageOfThreeNum {
+    public static int average(int a , int b, int c){
+        int average=(a+b+c)/3;
+
+        return average;
+    }
+    public static void main(String[] args) {
+        System.out.println(average(9, 4, 2));
+        
+    }
+    
+}
