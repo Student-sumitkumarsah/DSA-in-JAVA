a33fc949a3bf6ff1d8b8f58ee6e634ca1cdbfc12
sumit kumar sah <sumitkumar92365@gmail.com>
2024-08-22 23:02:13 +0530

    Hollow Rectangle Pattern

---
+++ DAY10/hollowRectangle.java
@@ -0,0 +1,23 @@
+package DSA.DAY10;
+
+public class hollowRectangle {
+    public static void hollow_rectangle(int row,int col){
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=col;j++){
+                if(i==1||i==row||j==1||j==col){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args) {
+        hollow_rectangle(4, 5);
+
+    }
+       
+    
+}
