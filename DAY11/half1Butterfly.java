c6a684a0f80fa7b6689e6be28762e931e95054de
sumit kumar sah <sumitkumar92365@gmail.com>
2024-09-03 23:31:19 +0530

    Upper Side butterfly Half part

---
+++ DAY11/half1Butterfly.java
@@ -0,0 +1,22 @@
+public class half1Butterfly {
+    public static void half1Butterfly(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        half1Butterfly(4);
+        
+    }
+    
+}
