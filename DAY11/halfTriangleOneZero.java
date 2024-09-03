51458ce6f20d06f99573004329cf586773e8651d
sumit kumar sah <sumitkumar92365@gmail.com>
2024-09-03 22:47:29 +0530

    Half Pyramid but print 1 and 0

---
+++ DAY11/halfTriangleOneZero.java
@@ -0,0 +1,20 @@
+public class halfTriangleOneZero {
+    public static void half(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        half(5);
+        
+    }
+    
+}
