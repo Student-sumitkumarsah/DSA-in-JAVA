9704a15b74a887d105bc0578e73b61c62951fa0e
sumit kumar sah <sumitkumar92365@gmail.com>
2024-10-02 21:15:15 +0530

    Traverse 2d Array

---
+++ DAY15/traverseTwoDiArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class traverseTwoDiArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int arr[][]=new int [row][col];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                    arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                    System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
