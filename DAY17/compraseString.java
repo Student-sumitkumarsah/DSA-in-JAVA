5fd0df9ae54f0652546bb49dc2ca2bbfdf1f7665
sumit kumar sah <sumitkumar92365@gmail.com>
2024-10-16 23:02:17 +0530

    Compress The Given String

---
+++ DAY17/compraseString.java
@@ -0,0 +1,22 @@
+public class compraseString {
+    public static void comprase(String str){
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
+            }
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) {
+        String str="aaabbbbcd";
+        comprase(str);
+    }
+    
+}
